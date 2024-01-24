242caa1501da563dcd65b12ec8dfc000a9938cf3
yamadahideto <yahideto428@gmail.com>
2024-01-24 23:34:06 +0900

    index.jsを修正

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,25 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function initialize() {
-    const location_data = JSON.parse(
-      document.getElementById("location_data").dataset.variable
-    );
-    // const latitude = 33.560655361380114;
-    // const longitud = 133.53351920699242;
-    const location = {
-      lat: location_data.latitude,
-      lng: location_data.longitud,
-    };
-    // const location = { lat: latitude, lng: longitud };
-    const panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("panorama"),
-      {
-        position: location,
-        pov: {
-          heading: 34,
-          pitch: 5,
-        },
-        addressControl: false, //住所情報の非表示
-      }
-    );
-  }
-});
+function initialize() {
+  const location_data = JSON.parse(
+    document.getElementById("location_data").dataset.variable
+  );
+  // const latitude = 33.560655361380114;
+  // const longitud = 133.53351920699242;
+  const location = { lat: location_data.latitude, lng: location_data.longitud };
+  // const location = { lat: latitude, lng: longitud };
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("panorama"),
+    {
+      position: location,
+      pov: {
+        heading: 34,
+        pitch: 5,
+      },
+      addressControl: false, //住所情報の非表示
+    }
+  );
+}
